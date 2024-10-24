c1eb573c778de0ba331b8e2e877e1b4ba75563dc
Alpaslan DOGAN <alpaslanndogan@hotmail.com>
2024-10-25 00:03:46 +0300

    image loader added for image url.

---
+++ ImageLoader.js
@@ -0,0 +1,3 @@
+export default function ImageLoader({ src }) {
+    return `https://alpaslan-datasance.github.io/datasanceWeb/${src}`;
+  }
--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
     basePath: "/datasanceWeb",
     output: "export",
     reactStrictMode: true,
+    experimental: {
+      appDir: true,
+    },
+    distDir: "out",
+    images: {
+      loader: "custom",
+      loaderFile: './ImageLoader.js'
+    },
   };
 
 export default nextConfig;
