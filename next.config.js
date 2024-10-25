9c109c2e3e96a506dbd6c24a60299fb7ce3992f2
Alpaslan DOGAN <alpaslanndogan@hotmail.com>
2024-10-25 13:38:17 +0300

    again

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-const hostnames = [
-  'alpaslan-datasance.github.io/datasanceWeb/']
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: hostnames.map(hostname => ({
-      protocol: 'https',
-      hostname
-    }))
-  }
-}
+    domains: ['alpaslan-datasance.github.io/datasanceWeb'],
+  },
+};
+
+export default nextConfig;
