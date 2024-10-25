d7105cca103b5733e96dd032564aa82b16593ce2
Alpaslan DOGAN <alpaslanndogan@hotmail.com>
2024-10-25 14:47:57 +0300

    changes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'Production';
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-  assetPrefix: '/datasanceWeb',
-  basePath: '/datasanceWeb',
-  output: 'export'
+  assetPrefix: isProd ? '/datasanceWeb' : "",
+  basePath: isProd ? '/datasanceWeb' : "",
+  output: 'export',
 };
 
 export default nextConfig;
